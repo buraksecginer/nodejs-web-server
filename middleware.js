04e3d869c081d1e1bf1c26e6bc5629f728f8846f
buraksecginer <brkscgnr@gmail>
2025-07-09 17:19:58 +0300

    Middleware modül olarak ayrıldı

---
+++ middleware.js
@@ -0,0 +1,11 @@
+var middleware = {
+    requireAuthentication: function(req, res, next) {
+        console.log('Kullanıcı doğrulaması yapılıyor...');
+        next();
+    },
+    logger: function(req, res, next) {
+        console.log(req.method + ' ' + req.originalUrl);
+        next();
+    }
+};
+module.exports = middleware;
